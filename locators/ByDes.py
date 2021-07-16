5cb1a20149bd0a24e2342a705e5557d48e58d68c
nailson13b <nailson.oliveira@outlook.com>
2021-07-15 22:43:53 -0300

    "update"

---
+++ locators/ByDes.py
@@ -0,0 +1,17 @@
+from appium import webdriver
+
+# Part 1 "Desired Capabilities"
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['platformVersion'] = '11'
+desired_caps['deviceName'] = 'Pixel 2 API 30'
+desired_caps['app'] = ('C:\workspace\Automation\Android_Appium_Demo.apk')
+desired_caps['appPackage'] = 'com.skill2lead.appiumdemo'
+desired_caps['appActivity'] = 'com.skill2lead.appiumdemo.MainActivity'
+
+# Part 2 "WebDriver object"
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+
+ele_des = driver.find_element_by_android_uiautomator('UiSelector().description("Btn3")')
+ele_des.click()
