0b54b4cbf4449c5e5e76bc5375728ea0d39cafe6
nailson13b <nailson.oliveira@outlook.com>
2021-07-18 00:02:29 -0300

    "update"

---
+++ Gestures/TapEx.py
@@ -0,0 +1,31 @@
+import time
+
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+
+# Part 1 "Desired Capabilities"
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['platformVersion'] = '11'
+desired_caps['deviceName'] = 'Pixel 2 API 30'
+desired_caps['app'] = ('C:\workspace\Automation\Android_Appium_Demo.apk')
+desired_caps['appPackage'] = 'com.skill2lead.appiumdemo'
+desired_caps['appActivity'] = 'com.skill2lead.appiumdemo.MainActivity'
+
+# Part 2 "WebDriver object"
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+
+wait = WebDriverWait(driver, 25, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException])
+
+ele = wait.until(lambda x: x.find_element_by_android_uiautomator('new UiScrollable(new UiSelector()).scrollIntoView(text("LONG CLICK"))'))
+
+actions = TouchAction(driver)
+actions.tap(None, 540, 1250, 1)
+actions.perform()
+
+time.sleep(2)
+driver.quit()
